981647aad24883b9a1bf06758a4f4a3af09e9931
Max Orok <maxwellorok@gmail.com>
2020-06-12 14:31:23 -0400

    reap files properly

--- read_files.c
+++ read_files.c
@@ -9,35 +9,15 @@
  * Read a number of files in parallel using io_uring
  *
  * g++ -Wall -O2 -o read_files read_files.c -luring
+ *
+ * -- Steps -- 
+ * 1. Create the ring
+ * 2. Check whether non-vectored read is supported using io_uring_probe
+ * 3. If so, submit read(v) operations
+ * 4. Reap read-completed completion queue entries
+ * 5. Tear-down
+ *
  */
-
-// #define QUEUE_DEPTH 4
-
-// class IoUring {
-// private:
-//     struct io_uring fRing;
-// public:
-//     IoUring() = delete;
-//     IoUring(uint32_t size, struct io_uring_params *params) : fRing() {}
-//     ~IoUring() {
-//         io_uring_queue_exit(&fRing);
-//     }
-//
-//     struct io_uring *ring() {
-//         return &fRing;
-//     }
-// }
-
-// 1. Create the ring
-// 2. Check whether non-vectored read is supported using io_uring_probe
-// 3. If so, submit read(v) operations
-// 4. Reap read-completed completion queue entries
-// 5. Tear-down
-
-//static int setup_context(unsigned entries, struct io_uring *ring) { 
-//}
-
-#define NUM_FILES 64
 
 struct RIOVec { 
 	void *fBuffer; 
@@ -51,7 +31,7 @@ struct ReadData {
 	struct RIOVec io_data; 
 }; 
 
-static int prep_reads(struct io_uring *ring, ReadData *files, int num_files) { 
+static int prep_reads(struct io_uring *ring, ReadData files[], int num_files) { 
 	struct io_uring_sqe *sqe; 
 	int fd = -1; 
 	for (int i = 0; i < num_files; i++) { 
@@ -66,26 +46,74 @@ static int prep_reads(struct io_uring *ring, ReadData *files, int num_files) {
 			return 1; 
 		}
 
-		io_uring_prep_read(sqe, fd, 0, 0, 0); 
+		io_uring_prep_read(sqe, fd, files[i].io_data.fBuffer, 
+		    files[i].io_data.fSize, files[i].io_data.fOffset); 
+		
+		// mark position in files array 
+		sqe->user_data = i; 
 	}
 	return 0; 
 }
 
-static int reap_reads(struct io_uring *ring, int num_files) { 
+static int reap_reads(struct io_uring *ring, ReadData files[], int num_files) { 
 	struct io_uring_cqe *cqe;
 	int ret; 
 
 	for (int i = 0; i < num_files; i++) { 
-		// won't hang, may return failure cqe 
+		// if right number of sqes are submitted, won't hang 
+		// but may return failure cqe 
+		// if too few sqes are submitted, could wait forever 
+		// -- switch to timeout 
 		ret = io_uring_wait_cqe(ring, &cqe); 
 		if (ret) { 
 			fprintf(stderr, "wait cqe: %d\n", ret); 
+			return 1; 
 		}
+		unsigned long index = (unsigned long) io_uring_cqe_get_data(cqe); 
+		if (index < 0 || index >= (unsigned long)num_files) { 
+			fprintf(stderr, "bad cqe user_data: %lu\n", index); 
+			return 1; 
+		}
+		if (cqe->res < 0) { 
+			fprintf(stderr, "read failed: %d\n", cqe->res); 
+			return 1; 
+		}
+		files[index].io_data.fOutBytes = (size_t)cqe->res; 
+		printf("read %lu bytes from file %lu\n", files[index].io_data.fOutBytes, index); 
+
+		// advance ring 
+		io_uring_cqe_seen(ring, cqe); 
 	}
 	return 0; 
 }
 
+// caller responsible for freeing using free_read_data
+#define BUF_SIZE 1024
+ReadData make_read_data(const char* pathname) { 
+	ReadData rd; 
+	rd.pathname = pathname; 
+	rd.io_data.fBuffer = malloc(BUF_SIZE); 
+	rd.io_data.fOffset = 0; 
+	rd.io_data.fSize = BUF_SIZE; 
+	rd.io_data.fOutBytes = 0; // set by cqe 
+	return rd; 
+}
+
+void free_read_data(ReadData *read_data) { 
+	if (NULL != read_data->io_data.fBuffer) { 
+		free(read_data->io_data.fBuffer); 
+	}
+}
+
+#define NUM_FILES 1
+
 int main(int argc, char* argv[]) {
+
+	//if (argc < 2) { 
+	//	printf("%s: file [files...]\n", argv[0]) ; 
+	//	return 1; 
+	//}	
+
     struct io_uring ring;
 	struct io_uring_probe *p; 
 	int ret; 
@@ -95,41 +123,43 @@ int main(int argc, char* argv[]) {
 	    fprintf(stderr, "ring create failed: %d\n", ret); 
 		return 1; 		
 	}
-	// kinda dumb, could do readv with length one 
-	// -- for simplicity
+	
+	// kinda dumb, could fallback to readv with length one 
+	// -- keep read for simplicity
 	p = io_uring_get_probe_ring(&ring); 
 	if (!p || !io_uring_opcode_supported(p, IORING_OP_READ)) { 
-	    fprintf(stderr, "read syscall not supported, exiting: %d\n", ret); 
+	    fprintf(stderr, "read op not supported by kernel, exiting: %d\n", ret); 
 		return 1; 
 	}
 	free(p); 
 	
 	ReadData files[NUM_FILES]; 
-	files[0].pathname = "file.txt"; 
-	files[0].io_data.fBuffer = NULL; 
-	files[0].io_data.fOffset = 0; 
-	files[0].io_data.fSize = 0; 
-	files[0].io_data.fOutBytes = 0; 
+	files[0] = make_read_data("file.txt"); 
+	if (files[0].io_data.fBuffer == NULL) { 
+		perror("malloc"); 
+		return 1; 
+	}
 
-	// ret = prep_reads(&ring, files, NUM_FILES); 
-	ret = prep_reads(&ring, files, 1);
+	ret = prep_reads(&ring, files, NUM_FILES); 
 	if (ret) {
 	    fprintf(stderr, "prep reads failed: %d\n", ret); 
 		return 1; 		
 	}
 	
 	ret = io_uring_submit(&ring); 
-	if (ret) {
-	    fprintf(stderr, "submit failed: %d\n", ret); 
+	if (ret <= 0) {
+	    fprintf(stderr, "submit sqe failed: %d\n", ret); 
 		return 1; 		
 	}
+	printf("submitted %d sqes\n", ret); 
 	
-	ret = reap_reads(&ring, NUM_FILES); 
+	ret = reap_reads(&ring, files, NUM_FILES); 
 	if (ret) {
 	    fprintf(stderr, "reap reads failed: %d\n", ret); 
 		return 1; 		
 	}
-
+	
+	free_read_data(&files[0]); 
     io_uring_queue_exit(&ring);
     return 0;
 }
